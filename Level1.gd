50d7c0454a8e802afaf17e660bd9160eef82b6ec
Nathan <nathanjwtx@nathanjwtx.io>
2020-07-24 21:35:51 -0500

    added function to test for solvability

--- Level1.gd
+++ Level1.gd
@@ -2,22 +2,38 @@ extends Node2D
 
 
 var initialPos: = []
-var tileNames:= []
+var tileNames: = []
 
 func _ready() -> void:
 	for t in get_node("Tiles").get_children():
 		initialPos.append(t.get_position())
-		# if t.name != "Tile1":
 		tileNames.append(t)
-		# else:
-			# t.queue_free()
 
 	set_starting_position()
+	test_solvability()
 
 func set_starting_position() -> void:
 	randomize()
 	initialPos.shuffle()
 	tileNames.shuffle()
 	for t in range(0, tileNames.size()):
-		# if tileNames[t].name != "Tile1":
 		tileNames[t].position = initialPos[t]
+		
+func test_solvability() -> void:
+	var _grid_width: = 3
+	var count: = 0
+	var tile: = 0
+	
+	for i in range(0, tileNames.size()):
+		# print("i: ", tileNames[i].name)
+		tile += 1
+		for j in range(tile, tileNames.size()):
+			# print("j: ", tileNames[j].name)
+			if tileNames[i].name > tileNames[j].name:
+				count += 1
+			
+	if count % 2 == 0:
+		print("solvable: ", count)
+	else:
+		print("not solvable: ", count)
+		test_solvability()
